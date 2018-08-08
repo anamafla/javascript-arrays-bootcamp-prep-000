f98263aa9d5207cc150ed29bf0ea565394dd52f7
Ana Mafla <anamafla@hotmail.com>
2018-08-08 14:08:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, newItem) {
-  let newArray = [newItem,...array];
+function addElementToBeginningOfArray(array, element) {
+  let newArray = [element,...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
