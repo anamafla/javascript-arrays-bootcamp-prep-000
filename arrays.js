07fd3f395c48b0623655c4a4761138c3cdf0538a
Ana Mafla <anamafla@hotmail.com>
2018-08-08 12:42:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,7 @@ function accessElementInArray(array, index){
 function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift();
 }
+
+function removeElementFromBeginningOfArray(){
+  
+}
